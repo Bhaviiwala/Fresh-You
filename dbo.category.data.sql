2fc240126b75cc10007719b38bd260c40c6d92dd
Bhaviiwala <158461066+Bhaviiwala@users.noreply.github.com>
2024-08-05 16:26:05 +0530

    Add files via upload

---
+++ dbo.category.data.sql
@@ -0,0 +1,6 @@
+﻿SET IDENTITY_INSERT [dbo].[category] ON
+INSERT INTO [dbo].[category] ([cid], [cname], [image]) VALUES (3, N'vegess', NULL)
+INSERT INTO [dbo].[category] ([cid], [cname], [image]) VALUES (4, N'fdgaaa', N'images (2).jpg')
+INSERT INTO [dbo].[category] ([cid], [cname], [image]) VALUES (6, N'fruits', N'images (4).jpg')
+INSERT INTO [dbo].[category] ([cid], [cname], [image]) VALUES (7, N'fresh fruits', N'images (1).jpg')
+SET IDENTITY_INSERT [dbo].[category] OFF
---
+++ dbo.product.data.sql
@@ -0,0 +1,111 @@
+﻿SET IDENTITY_INSERT [dbo].[product] ON
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (19, 16, 9, 9, N'Tender Coconut ', N'3.jpg', N'3.jpg', 51, 8, N'•	Coconuts are a source of quick energy and refreshment. They improve digestion and immune system health.
+•	They also prevent heart diseases by increasing good cholesterol and protect cells from cancer and ageing as they contain Cytokinin.
+', N'pcs', N',Diabetese Patients,Vitamin A,Vitamin C,Multivitamins,Skin Care')
+
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (33, 7, 20, 9, N' Lemons', N'l1.jfif', N'l1.jfif', 49, 6, N' on PinterestLemons contain vitamin C and other antioxidants that benefit health.
+Lemons are a citrus fruit that people often use in traditional remedies because of their health benefits. Like other citrus fruits, they contain vitamin C and other antioxidants.
+', N'kgs', N'vd')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (67, 9, 7, 7, N'Onion (Loose)', N'5.jpg', N'4.jpg', 29, 6, N'•	Onions are high in sulphur, Folic acid, Amino acid, vitamin B6 and B9. It has high quantities of water and naturally low in fat. It is high in phytochemical compounds.
+•	Onions are known to have antiseptic, antimicrobial and antibiotic properties which help you to get rid of infections.
+', N'kgs', N',Diabetese Patients,Weight Gain,Hair Care')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (72, 9, 7, 10, N'Cabbage ', N'27.jpg', N'27.jpg', 17, 5, N'•	Cabbage improves brain health and vision. Best for people who want to lose weight in a healthy way.
+•	It detoxifies the body and contains glutamine that reduces effects of inflammation, allergies, joint pain, irritation, fever.
+•	Cabbages also help prevent cancer.
+', N'pcs', N',Diabetese Patients,Multivitamins,Weight Loss')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (74, 9, 7, 7, N'Beetroot (Loose)', N'34.jpg', N'34.jpg', 16, 7, N'•	Red beets lower blood pressure, promotes brain and heart health.
+•	It fights inflammation and boosts energy levels.
+', N'kgs', N',Diabetese Patients,Multivitamins,Skin Care,Hair Care')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (76, 9, 7, 11, N'Raddish – white (Loose)', N'41.jpg', N'40.jpg', 24, 7, N'•	Radishes are excellent source of vitamin C.
+•	It contains folate, fiber, riboflavin, and potassium, as well as good amounts of copper, vitamin B6.
+', N'kgs', N',Vitamin C,Multivitamins')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (79, 7, 3, 11, N'Lemon Grass', N'14.jpg', N'13.jpg', 14, 7, N'Lemongrass is used for treating digestive tract spasms, stomach ache, high blood pressure, convulsions, pain, vomiting, cough, etc.
+It is also used to treat achy joints (rheumatism), fever, common cold, and exhaustion.
+Lemongrass is also used as a mild astringent.
+', N'kgs', N',Vitamin B,Vitamin E,Multivitamins')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (80, 7, 20, 9, N'Ginger – Mango ', N'15.jpg', N'16.jpg', 36, 8, N'
+Mango ginger is an appetizer and helps to cure allergies related to skin and itching and even bronchitis, asthma and inflammation.
+', N'kgs', N',Sugar Patients,Skin Care,Weight Gain,Hair Care')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (81, 7, 20, 6, N'Coriander Leaves', N'20.jpg', N'19.jpg', 10, 7, N'Coriander leaves fight food poisoning and lower blood sugar levels.
+They relieve urinary tract infections and help in maintaining a healthy menstrual cycle while preventing neurological inflammations and diseases.
+Tip - Add coriander leaves to boiling water. Let it cool down and then consume it. Drink this water every morning to cleanse the stomach.
+', N'kgs', N',Diabetese Patients,Sugar Patients,Multivitamins,Skin Care')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (82, 16, 9, 4, N'Tender Coconut ', N'3.jpg', N'3.jpg', 51, 8, N'•	Coconuts are a source of quick energy and refreshment. They improve digestion and immune system health.
+•	They also prevent heart diseases by increasing good cholesterol and protect cells from cancer and ageing as they contain Cytokinin.
+', N'pcs', N',Diabetese Patients,Vitamin A,Vitamin C,Multivitamins,Skin Care')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (83, 16, 15, 5, N'Garlic – Peeled', N'10.jpg', N'9.jpg', 89, 8, N'•	Garlic helps combat sickness, including the common cold and cough.
+•	They reduce blood pressure and the risk of heart diseases.
+•	Garlics also improve athletic performances and kills intestinal worms and harmful bacteria.
+', N'kgs', N',Sugar Patients,Vitamin E,Weight Loss')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (85, 16, 8, 3, N'Pomegranate - Peeled', N'13.jpg', N'13.jpg', 134, 7, N'•	Pomegranate juice lowers bad cholesterol and raises good cholesterol. Reduces the risk of heart stoke.
+•	Gives a boost of energy and freshness. So, this is specially recommended for women during menopause.
+•	Good for pregnant women as it prevents brain damage in infants.
+', N'kgs', N',Vitamin A,Vitamin C,Multivitamins,Skin Care')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (86, 16, 9, 3, N'Ash Gourd – Cut', N'21.jpg', N'21.jpg', 127, 6, N'•	Ash Gourd has loads of vitamins and minerals like phosphorus, calcium, riboflavin, iron, thiamine, niacin and Vitamin C.
+•	It has high moisture content and less fat, carbohydrates.
+•	The juice of raw ash gourd is used as a natural remedy to treat mild to severe dysentery.
+', N'kgs', N',Sugar Patients,Vitamin C,Vitamin D,Vitamin E')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (87, 16, 15, 6, N'Yam – Cut ', N'24.jpg', N'25.jpg', 65, 8, N'•	Yams are a good source of vitamin C.
+•	They also provide a good amount of fiber, potassium, manganese, and metabolic B vitamins.
+•	The content of vitamin B6 is good for shrinking the effects of homocysteine, which can do real damage to cell walls and ultimately lead to heart attacks and/or stroke.
+•	The crunchy edible root is chiefly composed of complex carbohydrates and soluble dietary fiber.
+', N'kgs', N',Vitamin C,Vitamin D,Vitamin E,Weight Loss,Hair Care')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (88, 16, 15, 6, N'Mint – Chopped', N'30.jpg', N'30.jpg', 125, 5, N'•	Mint is a remedy to manage ailments related to digestive tract, oral, respiratory and skin disorders such as acne, insect bites & burns.
+•	It is found to alleviate migraine pains, minor aches, muscle sprains and cramps.
+', N'kgs', N',Vitamin B,Vitamin E,Weight Loss')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (89, 16, 15, 9, N'Cut Cauliflower & Broccoli , Washed', N'31.jpg', N'33.jpg', 109, 7, N'•	One serving of cauliflower contains 77 percent of daily recommended value of vitamin C.
+•	Cauliflowers are rich in B complex, potassium and manganese.
+•	They protect from the risk of heart diseases and brain disorders.
+•	It also contains cancer fighting nutrient called sulforaphane.
+', N'kgs', N',Diabetese Patients,Vitamin D,Weight Loss,Hair Care')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (90, 16, 15, 11, N'Carrots - Grated', N'38.jpg', N'37.jpg', 70, 8, N'•	Carrots provide the highest content of vitamin A of all the vegetables.
+•	Brightly orange coloured carrots have pigments like carotenoids and flavonoids, that provide several antioxidants and act as a defense against cancer.
+•	Maintain oral health and also decreases the risk of stroke and other heart diseases.
+', N'kgs', N',Diabetese Patients,Sugar Patients,Skin Care,Hair Care')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (91, 10, 5, 3, N'Watermelon – Organically Grown (Loose)', N'of2.jpg', N'of3.jpg', 68, 9, N'•	Watermelons have excellent hydrating properties with 90% water content.
+•	Rich in anti-oxidant flavonoids that protects against prostate, breast, colon, pancreatic and lung cancers.
+•	They are an excellent source of lycopene which protects skin against harmful UV rays.
+', N'pcs', N',Vitamin C,Vitamin E,Skin Care')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (92, 10, 5, 4, N'Orange – Organically Grown (Loose)', N'of5.jpg', N'of5.jpg', 60, 7, N'•	Oranges are a good source of vitamin C and B complex.
+•	They lower cholesterol and control blood sugar level.
+•	Oranges contain carotenoids, which maintains good eye health and prevents constipation.
+•	Orange peels are highly nutritious, fiber-rich and edible. They are believed to be good for skin and nails.
+', N'kgs', N',Diabetese Patients,Vitamin A,Vitamin E')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (93, 10, 5, 7, N'Papaya - Organically Grown', N'of8.jpg', N'of10.jpg', 60, 7, N'•	Papayas reduce the risk of macular degeneration, a disease that affects the eyes as people age.
+•	They prevent asthma and cancer. Mashed papayas help in wound healing and preventing infections.
+•	The potassium and fiber content present in papayas help to ward off heart diseases.
+', N'pcs', N',Diabetese Patients,Sugar Patients,Vitamin A,Weight Gain')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (95, 10, 5, 4, N'Banana – Nendran Organically Grown ', N'of14.jpg', N'of16.jpg', 53, 7, N'•	Good for pregnant women and athletes.
+•	Powerhouse of nutrients
+•	Boosts the immune system.
+•	Helps in proper digestion.
+', N'kgs', N',Diabetese Patients,Sugar Patients,Multivitamins')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (96, 10, 5, 5, N'Mango – Tothapuri   Organically Grown ', N'of20.jpg', N'of18.jpg', 37, 7, N'Mangos are a good source of protective compounds with antioxidant properties, these plant chemicals include gallotannins and mangiferin.', N'kgs', N',Diabetese Patients,Vitamin A,Weight Loss,Hair Care')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (97, 9, 7, 5, N'Potato ', N'potato2.jpg', N'potato2.jpg', 21, 7, N'•	 Consumption of potatoes helps to maintain the blood glucose level and keeps the brain alert and active.
+•	Potatoes should be included in the diet of those having mouth ulcers. As they are easy to digest, they are good for patients.
+', N'kgs', N',Vitamin A,Vitamin D')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (98, 9, 7, 6, N'Tomato – Hybrid (Loose)', N'12.jpg', N'11.jpg', 19, 7, N'•	omatoes contain Vitamin C,K. lycopene, an antioxidant that reduces the risk of cancer and heart-diseases. They protect the eyes from light induced damage.
+•	Essential for pregnant women as these tomatoes protect infants against neural tube defects.
+
+', N'kgs', N',Vitamin B,Weight Loss')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (99, 9, 7, 6, N'Ladies’s Finger', N'ladishfinger2.jpg', N'ladishfinger1.jpg', 59, 8, N'•	Ladies'' finger aids in digestion as it contains prebiotic fiber and controls obesity and cholesterol.
+•	It is loaded with Vitamin A, B, C, and traces of Zinc & Calcium
+
+', N'kgs', N',Vitamin C,Multivitamins')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (100, 9, 10, 5, N'Amla (Loose)', N'ov5.jpg', N'ov1.jpg', 24, 7, N'The nutritional benefits of Amla include being rich in vitamin C, A and other similar ingredients, which help boost eye sight. They also cleanse the stomach and provide you with antioxidant benefits that help to keep the skin younger for a longer time and fights illnesses like cancer.
+
+', N'kgs', N',Diabetese Patients,Sugar Patients')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (101, 9, 10, 12, N'Tulsi Leaves', N'ov2.jpg', N'ov4.jpg', 45, 6, N'Tulsi Leaves are effective expectorants and promote the release of mucus in bronchitis and asthma. It relieves fever and help in expelling a kidney stone. It helps in the treatment of skin diseases such as ringworm and leucoderma. It may reduce blood cholesterol levels and have benefits for the heart. It acts a nerve tonic and improves brain functioning. It acts as anti-aging property.', N'kgs', N',Diabetese Patients,Vitamin B,Hair Care')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (102, 9, 10, 22, N'Turmeric - Organically Grown (Loose)', N'ov7.jpg', N'ov6.jpg', 20, 7, N'The fresh turmeric is known to have medicinal properties and is known to protect skin from bacteria, wrinkles and atmospheric pollution. Fresh turmeric is known to have both anti-inflammatory and antioxidant properties. When added to food, this helps to ease indigestion and even reduce heartburn caused by acidic influx.
+
+', N'kgs', N',Vitamin B,Multivitamins')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (103, 7, 3, 6, N'Lemon (Loose)', N'hb1.jpg', N'hb2.jpg', 57, 7, N'Lemon promotes hydration and keeps the skin healthy. They help ease digestion, prevents kidney stones and freshens breath. Lemons are a very good source of Vitamin C.
+Tip:-Lemon mixed with honey and water acts as a detoxifying agent
+', N'kgs', N',Multivitamins,Skin Care')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (104, 7, 20, 22, N'Curry leaves', N'hb4.jpg', N'hb3.jpg', 43, 7, N'•	Curry leaves are anti-diabetic and anti-carcinogenic.
+•	They help in the better functioning of the heart. they are a good remedy for diarrhea, nausea and indigestion.
+•	Curry leaves help improve eyesight and prevents cataract.
+•	Consume 4-5 raw curry leaves in the morning with an empty stomach to fight diabetes and grey hair.
+', N'kgs', N',Sugar Patients,Vitamin C')
+INSERT INTO [dbo].[product] ([pid], [cid], [subcid], [vid], [pname], [photo], [photo2], [price], [rates], [desc], [unit], [speciality]) VALUES (105, 7, 3, 6, N'Tomatoes', N'c1.jpg', N'b41.jpg', 44, 4, N'bjg', N'kgs', NULL)
+SET IDENTITY_INSERT [dbo].[product] OFF
